0ce6321095c882ff6a234aa86980c040f05721fe
uditdev1 <uditdev0008@gmail.com>
2026-03-12 22:33:26 +0530

    Added personalized custom quiz logic based on logged-in user

---
+++ js/customQuiz.js
@@ -0,0 +1,25 @@
+const user = JSON.parse(localStorage.getItem("user"));
+
+document.getElementById("welcomeUser").innerText =
+    `Welcome ${user.name}, here is your custom quiz`;
+
+const customQuestion = {
+    question: `${user.name}, which technology do you enjoy most?`,
+    options: ["HTML", "CSS", "JavaScript", "React"],
+};
+
+document.getElementById("customQuestion").innerText = customQuestion.question;
+
+const optionsContainer = document.getElementById("customOptions");
+
+customQuestion.options.forEach((option) => {
+    let btn = document.createElement("button");
+    btn.innerText = option;
+    btn.classList.add("option");
+
+    btn.onclick = () => {
+        alert(`${option} selected`);
+    };
+
+    optionsContainer.appendChild(btn);
+});
