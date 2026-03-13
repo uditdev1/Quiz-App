7728f20d250088f12dcb81c3e834c8cc4207e89e
uditdev1 <uditdev0008@gmail.com>
2026-03-13 22:47:03 +0530

    Integrated localStorage data into profile statistics

---
+++ js/profile.js
@@ -0,0 +1,19 @@
+const user = JSON.parse(localStorage.getItem("user"));
+
+if (!user) {
+    window.location.href = "login.html";
+}
+
+document.getElementById("profileName").innerText =
+`${user.name}'s Profile`;
+
+document.getElementById("totalQuiz").innerText =
+localStorage.getItem("totalQuiz") || 0;
+
+document.getElementById("lastScore").innerText =
+localStorage.getItem("lastScore") || 0;
+
+function logout() {
+    localStorage.removeItem("user");
+    window.location.href = "login.html";
+}
